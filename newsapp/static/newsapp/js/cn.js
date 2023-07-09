e7a434f516cb6ddf9947172d4a4322f58958b239
Nicolas Epstein <nilueps@gmail.com>
2023-07-08 22:46:54 -0400

    alpine

--- newsapp/static/newsapp/js/cn.js
+++
@@ -1,5 +0,0 @@
-function toggleComment(commentId) {
-	htmx.toggleClass(htmx.find(`#comment-${commentId}-children`), 'hidden');
-	// htmx.toggleClass(htmx.find(`#comment-${commentId}-children-toggler`), "after:content-['[_-_]']");
-	// htmx.toggleClass(htmx.find(`#comment-${commentId}-children-toggler`), "after:content-['[_+_]']");
-}
