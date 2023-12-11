abbdb93c07ac5bd6c0f99e68856c0be2e0ea5d09
Nicolas Epstein <nilueps@gmail.com>
2023-12-11 18:57:06 -0500

    remove ssl settings

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -2,9 +2,9 @@ from .common import *  # noqa
 
 DEBUG = False
 
-CSRF_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 
-SESSION_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 # HOST_SCHEME = "https"
