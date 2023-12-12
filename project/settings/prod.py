38541decc6b5bccd77e64c733870094168b30f63
Nicolas Epstein <nilueps@gmail.com>
2023-12-12 08:13:19 -0500

    get trusted origins from env

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -2,9 +2,10 @@ from .common import *  # noqa
 
 DEBUG = False
 
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
+
 # CSRF_COOKIE_SECURE = True
 
 # SESSION_COOKIE_SECURE = True
-
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
-# HOST_SCHEME = "https"
